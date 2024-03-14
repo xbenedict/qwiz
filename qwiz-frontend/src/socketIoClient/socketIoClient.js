12c08483f1b75ca280fa9b39e30679144a8342dd
Fadi Awwad <emailfadi@hotmail.com>
2024-03-14 08:49:16 +0300

    changed socketio url to qwiz.app

--- qwiz-frontend/src/socketIoClient/socketIoClient.js
+++ qwiz-frontend/src/socketIoClient/socketIoClient.js
@@ -4,7 +4,7 @@ let socket;
 
 export const initializeSocket = () => {
   if (!socket) {
-    socket = io("http://192.168.1.42:5000");
+    socket = io("http://qwiz.app:5000");
     //original socket ip below, use when developing locally
     // socket = io("http://192.168.1.42:5000");
   }
