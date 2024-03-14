afdc83f1ba771167d632a42e5de79f7784e6fe58
Fadi Awwad <emailfadi@hotmail.com>
2024-03-14 13:48:12 +0300

    first experimental branch to deploy online

--- frontend/src/socketIoClient/socketIoClient.js
+++ frontend/src/socketIoClient/socketIoClient.js
@@ -4,7 +4,10 @@ let socket;
 
 export const initializeSocket = () => {
   if (!socket) {
-    socket = io("http://qwiz.app:5000");
+    const port = process.env.PORT || 5000;
+
+    const url = `localhost:${port}`;
+    socket = io(url);
     //original socket ip below, use when developing locally
     // socket = io("http://192.168.1.42:5000");
   }
